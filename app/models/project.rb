77a3ad149a5d7088437b1843b2b084e3e77a69be
Rafa Ramírez <raffe90@gmail.com>
2015-12-05 12:25:01 +0100

    Adds Projects controller, projects#index

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,6 +3,14 @@ class Project < ActiveRecord::Base
     where(id: id).first
   end
 
+  def self.clean_old(too_old = Time.now.midnight - 1.week)
+    where("created_at < ?", too_old).destroy_all
+  end
+
+  def self.last_created_projects(limit = 10)
+    order(created_at: :desc).limit(limit)
+  end
+
   def iron_print
     "Project: #{name} - #{description}"
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   get '/' => 'site#home'
 
   get '/contact' => 'site#contact'
+
+  # get '/projects' => 'projects#index'
+
+  resources :projects
 end
